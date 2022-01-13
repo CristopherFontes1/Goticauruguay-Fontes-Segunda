985142112ee819ecc9c63f90668e93814e48676d
Cristopher Fontes <cris.fontstudy@gmail.com>
2022-01-13 15:00:25 -0300

    Cargado archivos de back end

---
+++ server/controllers/test.js
@@ -0,0 +1,12 @@
+//TESTER
+
+function test(req, res) {
+    res.send({
+        mensaje: 'API Funcionando, test exitoso',
+    });
+}
+
+
+module.exports = {
+    test
+}
--- src/app/Components/shared/components/footer/footer.component.ts
+++ src/app/Components/shared/components/footer/footer.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent   {
 
-  constructor() { }
+  title = 'express';
+  public test: string = '';
+  constructor(private httpClient: HttpClient) { }
+  
+  hacerTest() {
+    this.httpClient.get('/api/test/').subscribe(
+      (result: any) => {
+        console.log(result);
+        this.test = result.mensaje;
+      },
+      (err) => {this.test = 'Ha ocurrido un error en la petición del test'})}
 
-  ngOnInit(): void {
-  }
 
 }
+
